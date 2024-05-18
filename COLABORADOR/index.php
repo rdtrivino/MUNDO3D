ab56ec91f66adf24475a0e5f041ef784244f6dfd
aura2105 <auraale21@gmail.com>
2024-05-18 10:32:13 -0500

    Se ajusta ubicacion del menu lateral desplegable - Colaborador

--- COLABORADOR/index.php
+++ COLABORADOR/index.php
@@ -23,8 +23,8 @@
     <body class="sb-nav-fixed">
         <!--Inicia el codigo de la barra lateral izquierda de navegacion-->
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand ps-3" href="index.php">COLABORADOR</a>
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>        
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>  
+            <a class="navbar-brand ps-3" href="index.php">COLABORADOR</a>     
             <ul class="navbar-nav ms-auto ms-auto me-0 me-md-3 my-2 my-md-0">
                 <li class="nav-item dropdown">
                 <a class="nav-link dropdown" data-bs-toggle="" aria-expanded="">Bienvenid@ <?php echo $nombreCompleto; ?></a>
