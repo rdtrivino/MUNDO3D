1c595f1427cbfa6394659c98b960ca2f698e21ec
UBNUR-1851 <johanvalderrama1205@gmail.com>
2024-06-15 11:11:23 -0500

    Se ajusta boton de home en carrito de compras

--- carrito/index.php
+++ carrito/index.php
@@ -198,6 +198,77 @@ if (isset($_SESSION['user_id'])) {
             text-align: center;
             font-size: 18px;
         }
+
+        /* Boton Home */
+        .Btn-1 {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        width: 60px;
+        height: 60px;
+        border: none;
+        border-radius: 50%;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+        transition-duration: .3s;
+        box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.199);
+        background-color: rgb(0, 0, 0);
+        margin-top: 10px;
+        margin-left: 10px;
+        }
+
+        /* plus sign */
+        .sign {
+        width: 100%;
+        transition-duration: .3s;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        }
+
+        .sign svg {
+        width: 17px;
+        }
+
+        .sign svg path {
+        fill: white;
+        }
+        /* text */
+        .text {
+        position: absolute;
+        right: -10px; /* Ajusta la posición a la derecha, con un margen de 10px */
+        opacity: 0; /* Cambia la opacidad para hacer visible el texto */
+        color: white;
+        font-size: 1.2em;
+        font-weight: 600;
+        transition-duration: .3s;
+        }
+        /* hover effect on button width */
+        .Btn-1:hover {
+        width: 125px;
+        border-radius: 40px;
+        transition-duration: .3s;
+        }
+
+        .Btn-1:hover .sign {
+        width: 30%;
+        transition-duration: .3s;
+        padding-left: 20px;
+        }
+        /* hover effect button's text */
+        .Btn-1:hover .text {
+        opacity: 1;
+        width: 70%;
+        transition-duration: .3s;
+        padding-right: 10px;
+        }
+        /* button click effect*/
+        .Btn-1:active {
+        transform: translate(2px ,2px);
+        }
+        
+
     </style>
 </head>
 <style>
@@ -206,9 +277,14 @@ if (isset($_SESSION['user_id'])) {
 
     }
 </style>
-<a href="../USUARIO/Catalogologin.php" class="btn btn-link">
-    <i class="bi bi-house-door" style="color: black; font-size: 48px;"></i>
+
+<a class="Btn-1" href="../USUARIO/Catalogologin.php">
+    <div class="sign">
+        <img src="../images/iconizer-bx-home-alt-2.2.svg" alt="Inicio">
+    </div>
+    <div class="text">INICIO</div>
 </a>
+
 
 <div class="container">
     <div class="cuadro-global">
