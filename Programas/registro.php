4428eae954ef25d0e924a19b0758d3e5f4f819d9
Ysuarez93 <136213997+Ysuarez93@users.noreply.github.com>
2024-08-24 15:16:35 -0500

    Update registro.php

--- Programas/registro.php
+++ Programas/registro.php
@@ -1,8 +1,13 @@
 <?php
-$host = "localhost";
+/*$host = "localhost";
 $user = "root";
 $password = "";
-$dbname = "mundo3d";
+$dbname = "mundo3d";*/
+
+$host = "localhost";
+$user = "u255704174_root";
+$password = "Mundo3d2024";
+$dbname = "u255704174_mundo3d";
 
 // Establecer la conexión a la base de datos
 $link = mysqli_connect($host, $user, $password, $dbname);
