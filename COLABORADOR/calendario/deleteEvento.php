fb675fcb5d8b203ba5245b8d3dfa4f10db6c6a89
Ysuarez93 <136213997+Ysuarez93@users.noreply.github.com>
2024-10-02 19:06:07 -0500

    Delete COLABORADOR directory

--- COLABORADOR/calendario/deleteEvento.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('../../conexion.php');
-$id    		= $_REQUEST['id']; 
-
-$sqlDeleteEvento = ("DELETE FROM calendario WHERE  identificador='" .$id. "'");
-$resultProd = mysqli_query($link, $sqlDeleteEvento);
-
-?>
