b6493bdabdf4a10b2953b5961efae3b8cdd6e2ce
APRENDIZ <APRENDIZ@BOGDFORCDM076>
2024-10-03 19:55:16 -0500

    Ajuste de errores

--- ADMIN/tables.php
+++ ADMIN/tables.php
@@ -71,14 +71,18 @@
                     console.log('No se pudo determinar la tabla actual.');
                 }
             </script>
-            <ul class="navbar-nav ms-auto me-0 me-md-3 my-2 my-md-0">
+            <ul class="navbar-nav ms-auto ms-auto me-0 me-md-3 my-2 my-md-0">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false"><?php echo $nombreCompleto; ?><i
                             class="fas fa-user fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="../index.html" id="cerrar-sesion-button">Cerrar sesión</a>
+                        <li><a class="dropdown-item" href="confi.php">Configuracion de cuenta</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
                         </li>
+                        <li><a class="dropdown-item" href="../Programas/logout.php" id="cerrar-sesion-button">Cerrar
+                                sesión</a></li>
                     </ul>
                 </li>
             </ul>
